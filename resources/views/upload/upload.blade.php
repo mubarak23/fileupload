ad682b1d89459e50db6e027f936c6aca72238f8c
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-10-04 12:53:15 +0100

    setting up the view page

---
+++ resources/views/upload/upload.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Laravel file upload
+	</title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+</body>
+</html>
